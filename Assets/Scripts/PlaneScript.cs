4aacae71f56ac81cab942f77957f3f80681d5a25
daverave1212 <36113284+daverave1212@users.noreply.github.com>
2020-03-25 01:10:53 +0200

    Changed textures for tiles and made them different sizes.

--- Assets/Scripts/PlaneScript.cs
+++ Assets/Scripts/PlaneScript.cs
@@ -2,18 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlaneScript : MonoBehaviour
 {
 
     // Drag and drop the prefabs / materials here in Unity
     public GameObject tilePrefab;
-    public Material grassMaterial;
-    public Material waterMaterial;
-    public Material sandMaterial;
 
-    const float TILE_SIZE_X = 1.0f;
-    const float TILE_SIZE_Y = 0.25f;
-    const float TILE_SIZE_Z = 1.0f;
+    public static float TILE_SIZE_X = 1.0f;
+    public static float TILE_SIZE_Y = 0.25f;
+    public static float TILE_SIZE_Z = 1.0f;
 
     const int nTilesRows = 16;
     const int nTilesCols = 16;
@@ -26,15 +24,20 @@ public class PlaneScript : MonoBehaviour
         float topMostPoint = 0 - totalHeight / 2;
         float leftMostPoint = 0 - totalWidth / 2;
         tiles = new GameObject[nTilesRows, nTilesCols];
+        var rand = new System.Random();
         for (int row = 0; row<nTilesRows; row++) {
             for (int col = 0; col<nTilesCols; col++) {
                 var newTile = Instantiate(tilePrefab);
-                newTile.gameObject.transform.position = new Vector3(
-                    leftMostPoint + col * TILE_SIZE_X,
-                    TILE_SIZE_Y / 2,
-                    topMostPoint + row * TILE_SIZE_Z
-                );
-                newTile.GetComponent<Renderer>().material = grassMaterial;
+                var x = leftMostPoint + col * TILE_SIZE_X;
+                var y = TILE_SIZE_Y / 2;
+                var z = topMostPoint + row * TILE_SIZE_Z;
+                var type = "grass";
+                switch (rand.Next(1, 4)) {
+                    case 1: type = "grass"; break;
+                    case 2: type = "sand"; break;
+                    case 3: type = "water"; break;
+                }
+                newTile.GetComponent<TileScript>().Initialize(x, y, z, row, col, type);
                 tiles[row,col] = newTile;
             }
         }
