575d20f026223d4add6931e91e3f9d1fac6ad492
unknown <stanescualex98@gmail.com>
2020-06-03 16:10:47 +0300

    Buton Setari

---
+++ Assets/Scripts/SettingClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Panel;
+   public void OpenPanel()
+    {
+        if(Panel != null)
+        {
+            if (Panel.active == false)
+            {
+                Panel.SetActive(true);
+            }
+            else
+            {
+                Panel.SetActive(false);
+            }
+
+        }
+        
+    }
+}
